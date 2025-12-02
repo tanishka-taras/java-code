86aea9be666c57c5c0848bcbd8a7536788ffbea7
tanishka-taras <tanishkataras30@gmail.com>
2025-12-02 14:23:44 +0000

    3/12

---
+++ ScrollbarExample1.java
@@ -0,0 +1,25 @@
+import java.awt.*;   
+  
+public class ScrollbarExample1 {    
+  
+  
+ScrollbarExample1() {    
+  
+       
+            Frame f = new Frame("Scrollbar Example");    
+      
+            Scrollbar s = new Scrollbar();
+            s.setBounds (100, 100, 50, 100);  
+  
+   
+            f.add(s);    
+            f.setSize(400, 400);  
+            f.setLayout(null);    
+            f.setVisible(true);    
+}    
+  
+ 
+public static void main(String args[]) {    
+       new ScrollbarExample1();    
+}    
+}    

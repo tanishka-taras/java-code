5741e2bcce15492cfc22041b9f8b5c5e3fe7d4d0
tanishka-taras <tanishkataras30@gmail.com>
2025-12-10 13:56:02 +0000

    10//12

---
+++ OptionPaneSwing.java
@@ -0,0 +1,11 @@
+import javax.swing.*;  
+public class OptionPaneSwing {  
+JFrame f;  
+OptionPaneSwing(){  
+    f=new JFrame();  
+    JOptionPane.showMessageDialog(f,"Hello, Welcome to Javatpoint.");  
+}  
+public static void main(String[] args) {  
+    new OptionPaneSwing();  
+}  
+}  

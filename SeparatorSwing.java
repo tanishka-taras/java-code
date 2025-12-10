16216845e4334d4c1398a8bd062d7dc6b6cc0d28
tanishka-taras <tanishkataras30@gmail.com>
2025-12-10 14:09:20 +0000

    10/12

---
+++ SeparatorSwing.java
@@ -0,0 +1,24 @@
+import javax.swing.*;    
+class SeparatorSwing   
+{    
+          JMenu menu, submenu;    
+          JMenuItem i1, i2, i3, i4, i5;    
+          SeparatorSwing()  {    
+          JFrame f= new JFrame("Separator Example");    
+          JMenuBar mb=new JMenuBar();    
+          menu=new JMenu("Menu");    
+          i1=new JMenuItem("Item 1");    
+          i2=new JMenuItem("Item 2");       
+          menu.add(i1);  
+          menu.addSeparator();  
+          menu.add(i2);  
+          mb.add(menu);    
+          f.setJMenuBar(mb);    
+          f.setSize(400,400);    
+          f.setLayout(null);    
+          f.setVisible(true);    
+}    
+public static void main(String args[])    
+{    
+new SeparatorSwing();    
+}}    

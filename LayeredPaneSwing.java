82cee1f0717e288d7378db700431cac5dcea2f05
tanishka-taras <tanishkataras30@gmail.com>
2025-12-13 13:41:52 +0000

    13/12

---
+++ LayeredPaneSwing.java
@@ -0,0 +1,26 @@
+import javax.swing.*;  
+import java.awt.*;  
+public class LayeredPaneSwing extends JFrame {  
+  public LayeredPaneSwing() {  
+    super("LayeredPane Example");  
+    setSize(200, 200);  
+    JLayeredPane pane = getLayeredPane();  
+  
+    JButton top = new JButton();  
+    top.setBackground(Color.white);  
+    top.setBounds(20, 20, 50, 50);  
+    JButton middle = new JButton();  
+    middle.setBackground(Color.red);  
+    middle.setBounds(40, 40, 50, 50);  
+    JButton bottom = new JButton();  
+    bottom.setBackground(Color.cyan);  
+    bottom.setBounds(60, 60, 50, 50);    
+    pane.add(bottom, new Integer(1));  
+    pane.add(middle, new Integer(2));  
+    pane.add(top, new Integer(3));  
+  }  
+  public static void main(String[] args) {  
+      LayeredPaneSwing panel = new  LayeredPaneSwing();  
+      panel.setVisible(true);  
+  }  
+}  

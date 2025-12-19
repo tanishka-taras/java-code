ae19fe2136db2ff72ec92b9d65aa9b079e523089
tanishka-taras <tanishkataras30@gmail.com>
2025-12-19 14:25:12 +0000

    19/12

--- DisplayGraphics.java
+++ DisplayGraphics.java
@@ -1,26 +1,24 @@
-import java.awt.*;  
-import javax.swing.JFrame;  
-  
-public class DisplayGraphics extends Canvas{  
-      
-    public void paint(Graphics g) {  
-        g.drawString("Hello",40,40);  
-        setBackground(Color.BLUE);  
-        g.fillRect(130, 30,100, 80);  
-        g.drawOval(30,130,50, 60);  
-        setForeground(Color.PINK);  
-        g.fillOval(130,130,50, 60);  
-        g.drawArc(30, 200, 40,50,90,60);  
-        g.fillArc(30, 130, 40,50,180,40);  
-          
-    }  
-        public static void main(String[] args) {  
-        DisplayGraphics m=new DisplayGraphics();  
-        JFrame f=new JFrame();  
-        f.add(m);  
-        f.setSize(400,400);  
-       
-        f.setVisible(true);  
-    }  
-  
-}  
+import java.awt.*;
+import javax.swing.JFrame;
+
+public class DisplayGraphics extends Canvas{
+public void paint(Graphics g){
+g.drawString("Nice to meet you",60,60);
+setBackground(Color.WHITE);
+g.fillRect(190,40,100,80);
+g.drawOval(50,150,70,80);
+setForeground(Color.BLUE);
+g.fillOval(180,150,60,70);
+g.drawArc(30,200,40,50,90,60);      //x,y,w,h,sa,aa
+
+g.fillArc(30,130,40,50,0,180);     //x,y,w,h,sa,aa
+
+}                               
+public static void main(String[] args){
+DisplayGraphics dg=new DisplayGraphics();
+JFrame f = new JFrame();
+f.add(dg);
+f.setSize(400,400);
+f.setVisible(true);
+}
+}
